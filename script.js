cbe42ee3a15435327e95db5151fc98ec23fec215
Manalei97 <manal.ye.tr@gmail.com>
2022-10-21 15:04:51 +0300

    add window.alert

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-function animateElement()
-{
-    gsap.to(".box",{
-        x:'+=500',
-        y:'+=200',
+function animateElement() {
+    gsap.to(".box", {
+        x: '+=500',
+        y: '+=200',
         // width:'+=30'
         // height:'-=10'
         // rotate:'+=360',
-        duration:2,
+        duration: 2,
         // transformOrigin:'100% 50%',
         // scale:1.5,
         // repeat:2'
-        ease:"bounce.input"
+        ease: "bounce.input",
+        onComplete: () => {
+            window.alert('Done Manal')
+        }
     })
 }
